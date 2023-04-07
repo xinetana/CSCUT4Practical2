f504f1d4ca59ab3d2d5cc5b37d975cf725daa510
xinetana <124995307+xinetana@users.noreply.github.com>
2023-04-07 23:26:00 +0400

    Declares and Initializes Two objects

--- FilesInOut.java
+++ FilesInOut.java
@@ -11,24 +11,8 @@ import java.lang.Number;
  *
  */
 public class FilesInOut {
-
     public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
+        File inputFile = new File("C:\\Users\\Lenovo\\Downloads\\CSCUT4Practical2-main\\CSCUT4Practical2-main\\input.txt");
+        File outputFile = new File("output.txt");
 
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
-
-        // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
-
-        // Finally, add code to read the filenames as arguments from the command line.
-
-        System.out.println("You need to add your own code to do anything");
-
-    } // main
-
-} // FilesInOut
+    
